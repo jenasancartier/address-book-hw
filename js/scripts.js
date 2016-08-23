10a7468e6526160520423c95d1211a7a212f2c55
Jena SanCartier <jenasancartier@gmail.com>
2016-08-22 22:31:50 -0700

    add address constructor to make objects within objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,14 @@
 //business logic
-function Contact(first, last, street, city) {
+function Contact(first, last) {
   this.firstName = first;
   this.lastName = last;
+  this.addresses = [];
+}
+
+function Address(street, city, state) {
   this.street = street;
   this.city = city;
+  this.state = state;
 }
 
 Contact.prototype.fullName = function() {
